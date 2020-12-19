bed5155a086767c3b40ecdeddd429bff3bc33c67
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2020-12-19 19:08:21 +0900

    Add deployment workspace npx cdk init --language typescript

---
+++ deployment/bin/deployment.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { DeploymentStack } from '../lib/deployment-stack';
+
+const app = new cdk.App();
+new DeploymentStack(app, 'DeploymentStack');
