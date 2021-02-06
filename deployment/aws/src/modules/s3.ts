e54334325f197a12402fa9479c786a36148b25e1
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2021-02-06 19:37:38 +0900

    add s3 bucket module

---
+++ deployment/aws/src/modules/s3.ts
@@ -0,0 +1,23 @@
+import * as s3 from '@aws-cdk/aws-s3';
+import * as cdk from '@aws-cdk/core';
+import { Construct } from '@aws-cdk/core';
+
+import { Context } from '../types/context';
+
+type Props = {
+  bucketName: string;
+} & Context;
+
+export class S3 extends Construct {
+  constructor(parent: Construct, name: string, { bucketName, appName, env }: Props) {
+    super(parent, name);
+
+    new s3.Bucket(this, 'webBucket', {
+      bucketName: `${env}-${appName}-${bucketName}`,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'error.html',
+
+      removalPolicy: env === 'prd' ? cdk.RemovalPolicy.DESTROY : undefined,
+    });
+  }
+}
