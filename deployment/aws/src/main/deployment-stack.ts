3a075677ee7585d499b22946fb2ef2a0858ffd0b
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2021-02-06 21:15:11 +0900

    add region contex

--- deployment/aws/src/main/deployment-stack.ts
+++ deployment/aws/src/main/deployment-stack.ts
@@ -1,18 +1,21 @@
 import * as cdk from '@aws-cdk/core';
 
 import { S3 } from '../modules/s3';
+import { Context } from '../types/context';
 
 export class DeploymentStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const appName = this.node.tryGetContext('appName');
-    const env = this.node.tryGetContext('env');
+    const context: Context = {
+      appName: this.node.tryGetContext('appName'),
+      env: this.node.tryGetContext('env'),
+      region: this.node.tryGetContext('region'),
+    };
 
     new S3(this, 'StaticSite', {
       bucketName: 'web',
-      appName,
-      env,
+      ...context,
     });
   }
 }
--- deployment/aws/src/types/context.ts
+++ deployment/aws/src/types/context.ts
@@ -1,4 +1,5 @@
 export type Context = {
   appName: string;
   env: string;
+  region: string;
 };
